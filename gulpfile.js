35f696249391f2d9d1ffbf53f70bc908788400c0
gadarohan17 <gadarohan17@gmail.com>
2016-07-15 20:36:38 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
 var jsArray = [
-  './bower_components/jquery/dist/jquery.min.js',
-  './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-  './bower_components/flexslider/jquery.flexslider-min.js',
-  './bower_components/angular/angular.min.js',
-  './bower_components/angular-sanitize/angular-sanitize.min.js',
-  './bower_components/angular-animate/angular-animate.min.js',
-  './bower_components/angular-bootstrap/ui-bootstrap.min.js',
-  './bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-  './bower_components/ui-router/release/angular-ui-router.min.js',
-  './bower_components/angular-flexslider/angular-flexslider.js',
-  './js/app.js',
-  './js/controllers.js',
-  './js/templateservice.js',
-  './js/navigation.js',
+  "./bower_components/jquery/dist/jquery.js",
 
-  //please do not change it
-  './w/js/templates.js',
+  "./bower_components/angular/angular.js",
+  "./bower_components/angular-sanitize/angular-sanitize.min.js",
+  "./bower_components/ui-router/release/angular-ui-router.min.js",
+  "./bower_components/angular-aria/angular-aria.js",
+  "./bower_components/angular-animate/angular-animate.js",
+  "./bower_components/angular-material/angular-material.js",
+  "./bower_components/angular-material-icons/angular-material-icons.min.js",
+  "./bower_components/svg-morpheus/compile/minified/svg-morpheus.js",
+  "./bower_components/lodash/lodash.min.js",
+  "./bower_components/jStorage/jstorage.min.js",
+  "./bower_components/angular-clipboard/angular-clipboard.js",
+  "./bower_components/js-beautify/js/lib/beautify.js",
+  "./bower_components/angularjs-imageupload-directive/public/javascripts/imageupload.js",
+  "./bower_components/jquery-ui/jquery-ui.js",
+  "./bower_components/angular-ui-sortable/sortable.min.js",
+  "./bower_components/angular-bootstrap/ui-bootstrap.min.js",
+  "./bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
+
+  "./bower_components/ace-builds/src-noconflict/ace.js",
+  "./bower_components/ace-builds/src-noconflict/theme-monokai.js",
+  "./bower_components/ace-builds/src-noconflict/mode-json.js",
+
+  "./js/app.js",
+  "./js/controllers.js",
+  "./js/navigation.js",
+  "./js/templateservice.js",
+
+    //please do not change it
+    './w/js/templates.js',
 ];
 var replacehostFrom = "http://localhost/demo/";
 var replacehostTo = "http://wohlig.co.in/demo2/";
