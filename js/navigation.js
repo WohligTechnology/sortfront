25dcc5d5d2bf17e655d97e30c43287943bc53091
gadarohan17 <gadarohan17@gmail.com>
2016-07-23 13:50:29 +0530

    many fix

--- js/navigation.js
+++ js/navigation.js
@@ -65,13 +65,13 @@ var navigationservice = angular.module('navigationservice', [])
           $http.post(adminurl + apiName).success(successCallback).error(errorCallback);
       },
       submitLogin: function(data, successCallback, errorCallback) {
-          $http.post(adminurl + "register/login", data).success(successCallback).error(errorCallback);
+          $http.post(adminurl + "user/login", data).success(successCallback).error(errorCallback);
       },
       deleteApi: function(data, successCallback, errorCallback) {
           $http.post(adminURL + "api/delete", data).success(successCallback).error(errorCallback);
       },
       logout: function(successCallback, errorCallback) {
-          $http.post(adminurl + "register/logout").success(successCallback).error(errorCallback);
+          $http.post(adminurl + "user/logout").success(successCallback).error(errorCallback);
       },
       sideMenu1: function(apiName, pagination, successCallback, errorCallback) {
           $http.post(adminurl + apiName, pagination).success(successCallback).error(errorCallback);
